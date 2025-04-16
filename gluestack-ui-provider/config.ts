387c1b0087fd884cfe43407c7e50d75f334c1b30
marcosrodriguez00 <rodriguezmarcosdeveloper@gmail.com>
2025-04-16 11:12:30 -0400

    upgrade gluestack config

--- gluestack-ui-provider/config.ts
+++ gluestack-ui-provider/config.ts
@@ -1,3 +1,10 @@
 import { config as defaultConfig } from "@gluestack-ui/config";
+import { AnimationResolver } from "@gluestack-style/animation-resolver";
 
-export const config = defaultConfig;
+export const config = {
+  ...defaultConfig,
+  plugins: [
+    ...defaultConfig.plugins,
+    AnimationResolver,  // Agregar animationResolver en los plugins
+  ],
+};
